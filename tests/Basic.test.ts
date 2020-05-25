d3e2c09ccc39bf484415aa02ec3688d6084c0a3c
Liam Neville <lneville@quantumscape.com>
2020-05-25 12:06:48 -0700

    typescript mocha simple template

---
+++ tests/Basic.test.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+describe('Basic', () => {
+
+  beforeEach(() => {
+
+  })
+
+  
+  xit('Hello', () => {
+    const result : string = 'Hi Liam';
+    expect(result).to.equal('Hi Liam');
+  });
+
+});
